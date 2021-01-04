f3e6ff141933721e689b834274f679e5c3db1cc0
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2021-01-04 10:39:45 +0300

    Init

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('#slider-in').slick({
+  autoplay: true,
+  accessibility: false
+});
+$('#tweets-in').slick({
+  autoplay: true,
+  accessibility: false
+});
+$('#work-slide').slick({
+  autoplay: true,
+  accessibility: false,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  dots: true
+});
